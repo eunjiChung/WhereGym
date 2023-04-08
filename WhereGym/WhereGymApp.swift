4510ca46b372fbf942dd34be78e8d709eff6579d
eunjiChung <twih1203@gmail.com>
2023-04-09 02:01:43 +0900

    Initial Commit

---
+++ WhereGym/WhereGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereGymApp.swift
+//  WhereGym
+//
+//  Created by chungeunji on 2023/04/09.
+//
+
+import SwiftUI
+
+@main
+struct WhereGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
